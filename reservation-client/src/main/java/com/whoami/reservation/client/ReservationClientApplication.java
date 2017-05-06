536dec64b8015fe1410601a3a60a2831cdc07bc3
whoami <nothing.whoami@gmail.com>
2017-05-06 13:04:27 +0800

    [Whoami] - add loadBalance support.

--- reservation-client/src/main/java/com/whoami/reservation/client/ReservationClientApplication.java
+++ reservation-client/src/main/java/com/whoami/reservation/client/ReservationClientApplication.java
@@ -1,16 +1,94 @@
 package com.whoami.reservation.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableAutoConfiguration
+@Configuration
 @SpringBootApplication
 public class ReservationClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReservationClientApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
+
+@RestController
+@RequestMapping("/reservations")
+class ReservationApiGatewayRestController{
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("names")
+    public Collection<String> getReservationNames(){
+
+        ParameterizedTypeReference<Resources<Reservation>> ptr =
+                new ParameterizedTypeReference<Resources<Reservation>>(){};
+
+        ResponseEntity<Resources<Reservation>> responseEntity =
+                this.restTemplate.exchange("http://reservation-service/reservations",
+                        HttpMethod.GET, null, ptr);
+
+        Collection<String> nameList = responseEntity
+                .getBody()
+                .getContent()
+                .stream()
+                .map(Reservation::getReservationName)
+                .collect(Collectors.toList());
+
+        return nameList;
+    }
+}
+
+class Reservation{
+    private Long id;
+
+    private String reservationName;
+    public Reservation(){}
+
+    public Reservation(String reservationName) {
+        this.reservationName = reservationName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getReservationName() {
+        return reservationName;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Reservation{");
+        sb.append("id=").append(id);
+        sb.append(", reservationName='").append(reservationName).append("'}");
+        return sb.toString();
     }
 }
