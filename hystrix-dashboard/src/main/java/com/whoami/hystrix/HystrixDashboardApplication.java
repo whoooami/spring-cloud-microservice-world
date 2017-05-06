3e87b5b2492fda33d17657dd8716825a832f71bb
whoami <nothing.whoami@gmail.com>
2017-05-06 16:02:04 +0800

    [Whoami] - add hystrix support.

---
+++ hystrix-dashboard/src/main/java/com/whoami/hystrix/HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package com.whoami.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created by Whoami on 2017/5/6.
+ */
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
