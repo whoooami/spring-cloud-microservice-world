43c8c7b1adce616a109e2146bc8cfdf2753d75b3
whoami <nothing.whoami@gmail.com>
2017-05-08 13:59:49 +0800

    [Whoami] - add client to discovery.

--- config-service/src/main/java/com/whoami/config/ConfigServiceApplication.java
+++ config-service/src/main/java/com/whoami/config/ConfigServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServiceApplication {
 
