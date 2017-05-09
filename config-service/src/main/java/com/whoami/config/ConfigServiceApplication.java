e797088c62a93d511b597bba970244151c6e1ad7
whoami <nothing.whoami@gmail.com>
2017-05-09 15:37:13 +0800

    [Whoami] - config-sevice discovery is ok.

--- config-service/src/main/java/com/whoami/config/ConfigServiceApplication.java
+++ config-service/src/main/java/com/whoami/config/ConfigServiceApplication.java
@@ -3,8 +3,10 @@ package com.whoami.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigServiceApplication {
 
