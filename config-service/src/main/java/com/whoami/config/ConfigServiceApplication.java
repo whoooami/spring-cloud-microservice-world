114d1e1ccd9232dc4166b5aeccae0b87a132d849
whoami <nothing.whoami@gmail.com>
2017-05-09 11:00:50 +0800

    [Whoami] - config-service is fine.

--- config-service/src/main/java/com/whoami/config/ConfigServiceApplication.java
+++ config-service/src/main/java/com/whoami/config/ConfigServiceApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigServiceApplication {
 
