21a4880c2fa14d8e0c7c78c1dbfa745a28331056
whoami <nothing.whoami@gmail.com>
2017-05-16 17:20:47 +0800

    [Whoami] - add sms-service module.

---
+++ sms-service/src/main/java/com/whoami/sms/SmsServiceApplication.java
@@ -0,0 +1,15 @@
+package com.whoami.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Whoami on 2017/5/10.
+ */
+@SpringBootApplication
+public class SmsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsServiceApplication.class, args);
+    }
+}
---
+++ sms-service/src/main/java/com/whoami/sms/controller/MessageRestControler.java
@@ -0,0 +1,18 @@
+package com.whoami.sms.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class MessageRestControler {
+    @Value("${message}")
+    private String message;
+    
+    @RequestMapping("/message")
+    String message(){
+        return this.message;
+    }    
+}
