34c8ccfcfd6873f84d84a0d1a570ad6b84d473e3
whoami <nothing.whoami@gmail.com>
2017-05-23 14:19:44 +0800

    [Whoami] - change turbine to monitor, and this module is ok.

--- reservation-client/src/main/java/com/whoami/reservation/client/ReservationClientApplication.java
+++ reservation-client/src/main/java/com/whoami/reservation/client/ReservationClientApplication.java
@@ -32,7 +32,6 @@ import java.util.stream.Collectors;
 
 @Configuration
 @EnableAutoConfiguration
-@EnableZuulProxy
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 @EnableHystrix
--- reservation-client/src/main/java/com/whoami/reservation/feign/HelloFeignClientController.java
+++ reservation-client/src/main/java/com/whoami/reservation/feign/HelloFeignClientController.java
@@ -1,5 +1,6 @@
 package com.whoami.reservation.feign;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,11 @@ public class HelloFeignClientController {
     @Autowired
     private HelloClient helloClient;
 
+    public String helloFallBack() {
+        return "hello fall back.";
+    }
+
+    @HystrixCommand(fallbackMethod = "helloFallBack")
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         System.out.println("invoke remote hello api.");
