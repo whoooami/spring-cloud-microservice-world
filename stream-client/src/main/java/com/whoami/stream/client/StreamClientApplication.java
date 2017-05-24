c6984b4cef1939b5f02e4022a8da6d6ec98f2436
whoami <nothing.whoami@gmail.com>
2017-05-24 19:32:49 +0800

    [Whoami] - add stream module to discovey.

--- stream-client/src/main/java/com/whoami/stream/client/StreamClientApplication.java
+++ stream-client/src/main/java/com/whoami/stream/client/StreamClientApplication.java
@@ -2,10 +2,12 @@ package com.whoami.stream.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by Whoami on 2017/5/23.
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class StreamClientApplication {
     public static void main(String[] args) {
